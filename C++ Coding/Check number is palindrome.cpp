b0cbd977965d894055d67a64652aa8a564b57818
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-02 18:18:54 +0530

    palindrome

---
+++ C++ Coding/Check number is palindrome.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no,num,digit,rev=0;
+    cout <<"Enter a number to be checked "<<endl;
+    cin>>num;
+
+    no=num; // after reversing the number we will compare.
+
+    do{
+        digit=num%10;
+        rev=rev*10+digit;
+        num/=10;
+    }while(num!=0);
+
+    cout<<"The reverse of the number is "<<rev<<endl;
+
+    if (no==rev)
+    cout<<"The number is palindrome "<<endl;
+    else 
+    cout << "The number is not palindrome "<<endl;
+
+    return 0; 
+
+}
